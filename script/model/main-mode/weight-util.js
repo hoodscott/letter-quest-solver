850ac99c021a96b2d4bcd1d7b30e9b1a93c7033c
Meszerus <meszerus@gmail.com>
2021-04-01 16:34:25 +0100

    changing achievement weight value

--- script/model/main-mode/weight-util.js
+++ script/model/main-mode/weight-util.js
@@ -2,7 +2,7 @@ const MODIFIER_NONE = 0;
 const MODIFIER_STANDARD = 1;
 const MODIFIER_ACHIEVEMENT_COMMON = 3;
 const MODIFIER_ACHIEVEMENT_UNCOMMON = 5;
-const MODIFIER_ACHIEVEMENT_RARE = 20;
+const MODIFIER_ACHIEVEMENT_RARE = 40;
 const MODIFIER_ACHIEVEMENT_RARE_SPECIFIC = 50;
 
 const getAchievementWeight = (achievementWanted, conditionMet, rarityModifier) => {
