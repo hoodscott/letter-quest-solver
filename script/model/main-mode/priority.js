493bc40aab938770f6cea5fb781f683419157bb2
Meszerus <meszerus@gmail.com>
2021-04-18 15:37:43 +0100

    adding more priority conditions

--- script/model/main-mode/priority.js
+++ script/model/main-mode/priority.js
@@ -1,10 +1,22 @@
-const isCertainLength = (match, length) => match.word.length === length;
+const isExactLength = (match, length) => match.word.length === length;
+
+const isUpToLength = (match, length) => match.word.length <= length;
+
+const containsMultipleVowels = (match) =>
+    match.word.split('').filter(letter => ["a", "e", "i", "o", "u"].includes(letter)).length > 1;
+
+const isStartingWithVowel = (match) => ["a", "e", "i", "o", "u"].includes(match.word.charAt(0));
 
 const priorities = {
     "none": (_) => false,
-    "fiveLetterWords": (match) => isCertainLength(match, 5),
-    "sixLetterWords": (match) => isCertainLength(match, 6),
-    "sevenLetterWords": (match) => isCertainLength(match, 7),
+    "containsMultipleVowels": (match) => containsMultipleVowels(match),
+    "lengthOfFour": (match) => isExactLength(match, 4),
+    "lengthOfFive": (match) => isExactLength(match, 5),
+    "lengthOfFiveOrFewer": (match) => isUpToLength(match, 5),
+    "lengthOfSix": (match) => isExactLength(match, 6),
+    "lengthOfSeven": (match) => isExactLength(match, 7),
+    "startsWithConsonant": (match) => !isStartingWithVowel(match),
+    "startsWithVowel": (match) => isStartingWithVowel(match),
 }
 
 const isPrioritisedWord = (match, priority) => priorities[priority](match);
